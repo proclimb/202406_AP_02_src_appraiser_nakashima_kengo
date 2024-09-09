bf464ab20e0358800a61f969ed7bd0631098051f
nakashimakengo <202406ap01_nakashima@x-ide.sakura.ne.jp>
2024-09-09 11:00:55 +0900

    コメント挿入

--- SRC/class/stock/control.php
+++ SRC/class/stock/control.php
@@ -1,31 +1,31 @@
 <?php
-require ('class/stock/logic.php');
-require ('class/stock/model.php');
-require ('class/stock/view.php');
+require('class/stock/logic.php');
+require('class/stock/model.php');
+require('class/stock/view.php');
 
 function stock_control()
 {
     switch ($_REQUEST['act']) {
 
-        // 仕入管理
+            // 仕入管理
         case 'stock':
-        case 'stockSearch':
+        case 'stockSearch':   // 検索
             subStock();
             break;
 
-        case 'stockEdit':
+        case 'stockEdit':  //　編集
             subStockEdit();
             break;
 
-        case 'stockEditComplete':
+        case 'stockEditComplete':  //登録
             subStockEditComplete();
             break;
 
-        case 'stockDelete':
+        case 'stockDelete': //削除
             subStockDelete();
             break;
 
-        case 'stockListDelete':
+        case 'stockListDelete': //一括削除
             subStockListDelete();
             break;
     }
